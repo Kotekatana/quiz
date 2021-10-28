2eeb78f2054e9fee8594d2f7889383ab9513ce57
kotekatana <kotekatana@rambler.ru>
2021-10-28 15:20:25 +0300

    first commit

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'https://reactquiz-888ae-default-rtdb.europe-west1.firebasedatabase.app'
+})
---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import { Redirect } from "react-router";
+import { logout } from "../../store/actions/auth";
+import {connect} from 'react-redux'
+
+class Logout extends Component{
+    componentDidMount(){
+        this.props.logout()
+    }
+    render(){
+        return <Redirect to={'/'}/>
+    }
+}
+function mapDispatchToProps(dispatch){
+    return{
+        logout: () => dispatch(logout())
+    }
+}
+export default connect(null,mapDispatchToProps)(Logout)
